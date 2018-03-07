1877857f56063c13aa896180fcc82b93b1d86585
shaionbd <shakil.shaion@gmail.com>
2018-03-07 16:12:59 +0600

    Ok

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/enosis_business_solution/';
+$config['base_url'] = 'http://192.168.1.100/enosis_business_solution/';
 
 /*
 |--------------------------------------------------------------------------
